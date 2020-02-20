a5def327a94388abe274be7c8ce58ab9d64268f9
livevideo06 <61275148+livevideo06@users.noreply.github.com>
2020-02-20 19:32:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function playStream(idVideoTag,stream)
 }
 // openStrem()
 // .then(stream => playStream('localStream',stream));
-const peer = new Peer({key:'peerjs'});
+const peer = new Peer({key:'peerjs',host:'peerjs28.herokuapp.com',secure:true,port :443});
 peer.on('open',id =>{
     $('#btnSignUp').click(() =>
     {
